2d204e9c316800a70d7d27c2cda1bbe36792bf6f
Otosan1554 <52628890+Otosan1554@users.noreply.github.com>
2021-10-02 12:37:13 -0400

    Update initial_install3.sh

--- initial_install3.sh
+++ initial_install3.sh
@@ -1,65 +1,63 @@
 #!/usr/bin/bash
 
-mkdir /var/www/html/windows/
-mkdir /var/www/html/windows/script
-mkdir /var/www/html/linux/
-mkdir /var/www/html/linux/script
+mkdir /var/www/html/windows-script
+mkdir /var/www/html/linux-script
 
 echo "Linenumの導入"
-cd /var/www/html/linux/script
+cd /var/www/html/linux-script
 wget https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh  
 chmod 777 LinEnum.sh
 
 echo "LinPEASの導入"
-cd /var/www/html/linux/script
-wget https://github.com/carlospolop/PEASS-ng/blob/master/linPEAS/linpeas.sh
+cd /var/www/html/linux-script
+wget https://raw.githubusercontent.com/carlospolop/PEASS-ng/master/linPEAS/linpeas.sh
 chmod 777 linpeas.sh
 
 echo "linux exoloit suggester 2の導入"
-cd /var/www/html/linux/script
+cd /var/www/html/linux-script
 wget https://raw.githubusercontent.com/jondonas/linux-exploit-suggester-2/master/linux-exploit-suggester-2.pl
 chmod 777 linux-exploit-suggester-2.pl
 
 echo "Linux priv checkerの導入"
-cd /var/www/html/linux/script
+cd /var/www/html/linux-script
 wget https://raw.githubusercontent.com/sleventyeleven/linuxprivchecker/master/linuxprivchecker.py
 chmod 777 linuxprivchecker.py
 
 echo "Linux Smart Enumeration Toolの導入"
-cd /var/www/html/linux/script
+cd /var/www/html/linux-script
 wget https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/master/lse.sh    
 chmod 777 lse.sh
 
 echo "Unix privesc checkerの導入"
-cd /var/www/html/linux/script
-wget https://github.com/pentestmonkey/unix-privesc-check/blob/1_x/unix-privesc-check
+cd /var/www/html/linux-script
+wget https://raw.githubusercontent.com/pentestmonkey/unix-privesc-check/1_x/unix-privesc-check
 mv unix-privesc-check unix-privesc-check.sh
-chmod 777 /root/www/script/unix-privesc-check.sh
+chmod 777 unix-privesc-check.sh
 
-echo "windows-privsesc-checkの導入"
-cd /var/www/html/windows/script
-wget https://github.com/pentestmonkey/windows-privesc-check/blob/master/windows-privesc-check2.exe
+echo "Linux Scriptの導入"
+cd /var/www/html/linux-script
+wget https://raw.githubusercontent.com/Otosan1554/kali_conf/main/linux-script/script_linux.sh
+chmod 777 script_linux.sh
 
 echo "winPEASの導入"
-cd /var/www/html/windows/script
+cd /var/www/html/windows-script
 wget https://github.com/carlospolop/PEASS-ng/blob/master/winPEAS/winPEASexe/binaries/x64/Release/winPEASx64.exe
 wget https://github.com/carlospolop/PEASS-ng/blob/master/winPEAS/winPEASexe/binaries/x86/Release/winPEASx86.exe
-wget https://github.com/carlospolop/PEASS-ng/blob/master/winPEAS/winPEASbat/winPEAS.bat
+wget https://github.com/carlospolop/PEASS-ng/blob/master/winPEAS/winPEASexe/binaries/Release/winPEASany.exe
 
-echo "Windows exoloit suggesterの導入"
-cd /root/tools
-git clone https://github.com/bitsadmin/wesng
+echo "windows-privsesc-checkの導入"
+cd /var/www/html/windows-script
+wget https://github.com/pentestmonkey/windows-privesc-check/blob/master/windows-privesc-check2.exe
 
 echo "JAWSの導入"
-cd /var/www/html/windows/script
-wget https://github.com/411Hall/JAWS/blob/master/jaws-enum.ps1
+cd /var/www/html/windows-script
+wget https://raw.githubusercontent.com/411Hall/JAWS/master/jaws-enum.ps1
 
-echo "root/www配下を作成"
+echo "var/www/html配下を作成"
 cp -R /usr/share/windows-resources /var/www/html/windows-resources 
 cp -R /usr/share/windows-binaries /var/www/html/windows-binaries 
 cp -R /usr/share/webshells /var/www/html/webshells 
 cp -R /usr/share/nishang /var/www/html/nishang
-mv /root/kali_conf/script_linux /var/www/html/linux/script
 mv /root/kali_conf/webshell/* /var/www/html/webshells
 mv /root/kali_conf/windows-binaries/Seatbelt.exe /var/www/html/windows/script
 mv /root/kali_conf/windows-binaries/SharpUp.exe /var/www/html/windows/script
@@ -70,3 +68,32 @@ sudo apt install powercat
 
 echo "Powersploitの導入"
 apt install powersploit
+
+
+#################################### /privesc #########
+
+
+
+
+
+
+echo "root/www配下を作成"
+cp -R /usr/share/windows-resources /root/www/windows-resources 
+cp -R /usr/share/windows-binaries /root/www/windows-binaries 
+cp -R /usr/share/windows-privesc-check /root/www/windows-privesc-check 
+cp -R /usr/share/webshells /root/www/webshells 
+cp -R /usr/share/nishang /root/www/nishang
+cp -R /usr/share/unix-privesc-check /root/www/unix-privesc-check 
+mv /root/kali_conf/webshell/* /root/www/webshells
+mv /root/kali_conf/script/* /root/www/script
+cp /root/www/windows-resources/powersploit/Privesc/PowerUp.ps1 /root/www/script
+
+
+echo "BeRootの導入"
+cd /var/www/html/windows-script
+wget https://github.com/AlessandroZ/BeRoot/blob/master/Linux/beroot.py
+chmod 777 beroot.py
+
+echo "Windows exoloit suggesterの導入"
+cd /root/tools
+git clone https://github.com/bitsadmin/wesng
