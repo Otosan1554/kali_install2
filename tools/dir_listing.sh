2a8271df7d8a8ab247af1c60cb71bd3c15dbd11d
Otosan1554 <52628890+Otosan1554@users.noreply.github.com>
2021-11-04 22:02:35 -0400

    Add files via upload

--- tools/dir_listing.sh
+++ tools/dir_listing.sh
@@ -19,21 +19,21 @@ else
    echo ""
 fi
 
-rm /root/Lab/$IP/dirbuster/url_level$level_new'_temp'
-rm /root/Lab/$IP/dirbuster/wordlist_level$level_new
-rm /root/Lab/$IP/dirbuster/not_found_level$level_new
-rm /root/Lab/$IP/dirbuster/url_level$level_new
+rm /tmp/dirbuster_$IP/url_level$level_new'_temp'
+rm /tmp/dirbuster_$IP/wordlist_level$level_new
+rm /tmp/dirbuster_$IP/not_found_level$level_new
+rm /tmp/dirbuster_$IP/url_level$level_new
 
-touch /root/Lab/$IP/dirbuster/wordlist_level$level_new
-touch /root/Lab/$IP/dirbuster/not_found_level$level_new
-touch /root/Lab/$IP/dirbuster/url_level$level_new
+touch /tmp/dirbuster_$IP/wordlist_level$level_new
+touch /tmp/dirbuster_$IP/not_found_level$level_new
+touch /tmp/dirbuster_$IP/url_level$level_new
 
-sort -u /root/Lab/$IP/dirbuster/url_level$level
+sort -u /tmp/dirbuster_$IP/url_level$level
 
 count=1
-sousu=$(cat /root/Lab/$IP/dirbuster/url_level$level | wc -l)
+sousu=$(cat /tmp/dirbuster_$IP/url_level$level | wc -l)
 
-for url in $(cat /root/Lab/$IP/dirbuster/url_level$level)
+for url in $(cat /tmp/dirbuster_$IP/url_level$level)
 do
    len=${#url}
    if [ ${url:$len-1:1} != '/' ]; then
@@ -41,36 +41,36 @@ do
    fi
    echo "<<<   Start Level$level $count/$sousu  $url'FUZZ'   >>>"
    count=$((count += 1))
-   ffuf -s -w /root/Lab/$IP_m/dirbuster/common.txt -mc 200,204,301,302,307,401,403,405 -t 40 -u  $url'FUZZ' > /root/Lab/$IP/dirbuster/url_level$level_new'_temp'
-   for url1 in $(cat /root/Lab/$IP/dirbuster/url_level$level_new'_temp')
+   ffuf -s -w /tmp/dirbuster_$IP/common.txt -mc 200,204,301,302,307,401,403,405 -t 40 -u  $url'FUZZ' > /tmp/dirbuster_$IP/url_level$level_new'_temp'
+   for url1 in $(cat /tmp/dirbuster_$IP/url_level$level_new'_temp')
    do
       flg='n'
       if [ $url1 = 'manual' ]; then
          read -p 'Skip manual ? > ' flg
       fi
       if [ $flg = 'n' -o $flg = 'N' ]; then 
-         echo $url1 > /root/Lab/$IP/dirbuster/dirb_wordlist_temp
-         cat /root/Lab/$IP/dirbuster/dirb_wordlist_temp
-         dirb  $url /root/Lab/$IP/dirbuster/dirb_wordlist_temp > /root/Lab/$IP/dirbuster/dirb_result_temp
-         hantei1=$(cat /root/Lab/$IP/dirbuster/dirb_result_temp | grep -i 'LISTABLE' | wc -l)
-         hantei2=$(cat /root/Lab/$IP/dirbuster/dirb_result_temp | grep -i 'directory' | wc -l)
-         hantei3=$(cat /root/Lab/$IP/dirbuster/dirb_result_temp | grep -i 'CODE:' | wc -l)
+         echo $url1 > /tmp/dirbuster_$IP/dirb_wordlist_temp
+         cat /tmp/dirbuster_$IP/dirb_wordlist_temp
+         dirb  $url /tmp/dirbuster_$IP/dirb_wordlist_temp > /tmp/dirbuster_$IP/dirb_result_temp
+         hantei1=$(cat /tmp/dirbuster_$IP/dirb_result_temp | grep -i 'LISTABLE' | wc -l)
+         hantei2=$(cat /tmp/dirbuster_$IP/dirb_result_temp | grep -i 'directory' | wc -l)
+         hantei3=$(cat /tmp/dirbuster_$IP/dirb_result_temp | grep -i 'CODE:' | wc -l)
          if [ $hantei1 != 0 -o $hantei2 != 0 -o $hantei3 != 0 ]; then
-            echo $url1 >> /root/Lab/$IP/dirbuster/wordlist_level$level_new
+            echo $url1 >> /tmp/dirbuster_$IP/wordlist_level$level_new
             if [ $hantei1 = 0 -a $hantei2 != 0 ]; then
 #            if [ $hantei1 != 0 -o $hantei2 != 0 ]; then
-               echo $url$url1 >> /root/Lab/$IP/dirbuster/url_level$level_new
+               echo $url$url1 >> /tmp/dirbuster_$IP/url_level$level_new
             fi
          else
-            echo $url$url1 >> /root/Lab/$IP/dirbuster/not_found_level$level_new
+            echo $url$url1 >> /tmp/dirbuster_$IP/not_found_level$level_new
          fi
       fi
    done
 done
 
-rm /root/Lab/$IP/dirbuster/url_level$level_new'_temp'
-rm /root/Lab/$IP/dirbuster/dirb_wordlist_temp
-rm /root/Lab/$IP/dirbuster/dirb_result_temp
+rm /tmp/dirbuster_$IP/url_level$level_new'_temp'
+rm /tmp/dirbuster_$IP/dirb_wordlist_temp
+rm /tmp/dirbuster_$IP/dirb_result_temp
 
 }
 
@@ -81,10 +81,10 @@ IP_m=$(echo $url_m | cut -d "/" -f 3 | cut -d ":" -f 1 )
 port=$(echo $url_m | cut -d "/" -f 3 | cut -d ":" -f 2 )
 addurl=$(echo $url_m | cut -d "/" -f 4)
 
-echo $protocol
-echo $IP_m
-echo $port
-echo $addurl
+echo "protocol: " $protocol
+echo "IP Address: " $IP_m
+echo "port: " $port
+echo "Addurl: "$addurl
 
 if [ ${url_m:0:7} = 'http://' ] ; then
    echo ""
@@ -112,52 +112,56 @@ else
    exit
 fi
 
-if [ -d /root/Lab/$IP_m/dirbuster ]; then
+if [ -d /tmp/dirbuster_$IP_m ]; then
    echo ""
 else
-   mkdir /root/Lab/$IP_m/dirbuster
+   mkdir /tmp/dirbuster_$IP_m
 fi
 
+echo "<------------------- Robots.txt ------------------------->"
 curl $protocol://$IP_m/$port/robots.txt
+echo "<------------------- Robots.txt ------------------------->"
 
-cp /usr/share/seclists/Discovery/Web-Content/common.txt /root/Lab/$IP_m/dirbuster/common.txt 
+cp /usr/share/seclists/Discovery/Web-Content/common.txt /tmp/dirbuster_$IP_m/common.txt 
 
 entry='x'
 while [ "$entry" != "" ]
 do
-   read -p 'Entry Add ? > ' entry
+   read -p 'Search String Entry Add ? > ' entry
    if [ "$entry" != "" ]; then
-      echo entry >> /root/Lab/$IP_m/dirbuster/common.txt
+      echo entry >> /tmp/dirbuster_$IP_m/common.txt
    fi
 done
+
+# saigo ga / de owatteru?
 
 len_m=${#url_m}
 if [ ${url_m:$len_m-1:1} != '/' ]; then
    url_m=$url_m/
 fi
 
-echo $url_m > /root/Lab/$IP_m/dirbuster/url_level0
+echo $url_m > /tmp/dirbuster_$IP_m/url_level0
 
 rm /root/Lab/$IP/dirbuster/wordlist_temp
 
 wlistgen $IP_m 0
-cat /root/Lab/$IP_m/dirbuster/wordlist_level1 > /root/Lab/$IP_m/dirbuster/wordlist_temp
+cat /tmp/dirbuster_$IP_m/wordlist_level1 > /tmp/dirbuster_$IP_m/wordlist_temp
 wlistgen $IP_m 1
-cat /root/Lab/$IP_m/dirbuster/wordlist_level2 >> /root/Lab/$IP_m/dirbuster/wordlist_temp
+cat /tmp/dirbuster_$IP_m/wordlist_level2 >> /tmp/dirbuster_$IP_m/wordlist_temp
 wlistgen $IP_m 2
-cat /root/Lab/$IP_m/dirbuster/wordlist_level3 >> /root/Lab/$IP_m/dirbuster/wordlist_temp
+cat /tmp/dirbuster_$IP_m/wordlist_level3 >> /tmp/dirbuster_$IP_m/wordlist_temp
 wlistgen $IP_m 3
-cat /root/Lab/$IP_m/dirbuster/wordlist_level4 >> /root/Lab/$IP_m/dirbuster/wordlist_temp
+cat /tmp/dirbuster_$IP_m/wordlist_level4 >> /tmp/dirbuster_$IP_m/wordlist_temp
 wlistgen $IP_m 4
-cat /root/Lab/$IP_m/dirbuster/wordlist_level5 >> /root/Lab/$IP_m/dirbuster/wordlist_temp
+cat /tmp/dirbuster_$IP_m/wordlist_level5 >> /tmp/dirbuster_$IP_m/wordlist_temp
 
 
-rm /root/Lab/$IP_m/Scan/dirb/Scan/tcp_$port'_'$protocol'_dirb'$addurl'.txt'
+rm /tmp/dirbuster_$IP_m/tcp_$port'_'$protocol'_dirb'$addurl'.txt'
 
-sort -u /root/Lab/$IP_m/dirbuster/wordlist_temp
+sort -u /tmp/dirbuster_$IP_m/wordlist_temp
 
-dirb $url_m /root/Lab/$IP_m/dirbuster/wordlist_temp -o /root/Lab/$IP_m/Scan/tcp_$port'_'$protocol'_dirb'$addurl'.txt'
+dirb $url_m /tmp/dirbuster_$IP_m/wordlist_temp -o /tmp/dirbuster_$IP_m/tcp_$port'_'$protocol'_dirb'$addurl'.txt'
 
-rm /root/Lab/$IP_m/dirbuster/wordlist_temp
+rm /tmp/dirbuster_$IP_m/wordlist_temp
 
 
