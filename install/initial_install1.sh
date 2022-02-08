b812f2df587440a3a6bad6213051a4d0dd40c6a2
Otosan1554 <52628890+Otosan1554@users.noreply.github.com>
2022-02-09 00:06:08 +0900

    Update initial_install1.sh

--- install/initial_install1.sh
+++ install/initial_install1.sh
@@ -5,26 +5,34 @@ read -p "アンチウィルスを停止しましたか？"
 echo "ソースリストの更新"
 sudo apt -y update 
 
-echo "seclistsの導入"
-sudo apt-get install seclists
-
 read -p "kali/kaliにログインします"
 read -p "sudo passwd root します"
 read -p "キーボードに日本語を選択します"
-read -p "タイムゾーンを変更します"
-read -p "terminalの背景を白にします(一般+root用)"
+read -p "右上のタイム表示を右クリックし、プロパティでタイムゾーンを変更します"
+read -p "terminalの背景を白にします"
 read -p "エディタ(mousepad)の背景を白にします"
-read -p "スクリーンセーバーを非有効化します"
-read -p "Github、TryHackMeのログイン情報を登録します"
+read -p "エディタ(texteditor)の背景を白にします"
+read -p "Settings>Power Manger>Securityタブからスクリーンセーバーを非有効化します"
 read -p "smb.confのglobalセクションに client min protocol = core を追加します"
 read -p "Firefoxのブラウザアドオンを導入します Web Developer/Tamper Data/Foxy Proxy/User Agent Switcher/Wappalyzer"
-read -p "FoxyProxyを設定する"
+read -p "FoxyProxyを設定します"
 read -p "Burpの証明書をFirefoxに設定します"
 read -p "Github、TryHackMeのログイン情報を登録します"
 
-read -p "root/rootにログインします"
+read -p "rootにログインします"
 read -p "キーボードに日本語を選択します"
+read -p "右上のタイム表示を右クリックし、プロパティでタイムゾーンを変更します"
+read -p "terminalの背景を白にします"
+read -p "エディタ(mousepad)の背景を白にします"
+read -p "エディタ(texteditor)の背景を白にします"
+read -p "Settings>Power Manger>Securityタブからスクリーンセーバーを非有効化します"
+read -p "Firefoxのブラウザアドオンを導入します Web Developer/Tamper Data/Foxy Proxy/User Agent Switcher/Wappalyzer"
+read -p "FoxyProxyを設定します"
+read -p "Burpの証明書をFirefoxに設定します"
+read -p "Github、TryHackMeのログイン情報を登録します"
 
+echo "seclistsの導入"
+sudo apt-get install seclists
 
 
 #echo "OpenVASのインストールと起動"
