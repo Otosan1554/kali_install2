473b1a084d99360856029454f583b117ee9ead16
Otosan1554 <52628890+Otosan1554@users.noreply.github.com>
2022-08-25 12:49:40 +0900

    Update initial_install0.sh

--- install/initial_install0.sh
+++ install/initial_install0.sh
@@ -2,10 +2,10 @@
 
 cd /tmp
 
-wget https://raw.githubusercontent.com/Otosan1554/kali_conf/main/install/initial_install1.sh
-wget https://raw.githubusercontent.com/Otosan1554/kali_conf/main/install/initial_install2.sh
-wget https://raw.githubusercontent.com/Otosan1554/kali_conf/main/install/initial_install3.sh
-wget https://raw.githubusercontent.com/Otosan1554/kali_conf/main/install/initial_install4.sh
+wget https://raw.githubusercontent.com/Otosan1554/kali_install2/main/install/initial_install1.sh
+wget https://raw.githubusercontent.com/Otosan1554/kali_install2/main/install/initial_install2.sh
+wget https://raw.githubusercontent.com/Otosan1554/kali_install2/main/install/initial_install3.sh
+wget https://raw.githubusercontent.com/Otosan1554/kali_install2/main/install/initial_install4.sh
 
 chmod 777 initial_install1.sh
 chmod 777 initial_install2.sh
