4aa68a611a9131f7294529a377a717cc7167a494
Otosan1554 <52628890+Otosan1554@users.noreply.github.com>
2022-08-25 00:36:43 -0400

    Update initial_install1.sh

--- install/initial_install1.sh
+++ install/initial_install1.sh
@@ -2,6 +2,7 @@
 
 read -p "メモリを追加しましたか？"
 read -p "アンチウィルスを停止しましたか？"
+read -p "rootにログインしましたか？"
 
 echo "ソースリストの更新"
 sudo apt -y update 
@@ -13,13 +14,17 @@ chmod 777 pimpmykali.sh
 read -p "select N, Y, Y, Y"
 ./pimpmykali.sh
 
-#echo "chromeの導入"
-#cd /tmp
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#apt install ./google-chrome-stable_current_amd64.deb
-
-#echo "geditの導入"
-#apt install gedit
+read -p "キーボードに日本語を選択します"
+read -p "右上のタイム表示を右クリックし、プロパティでタイムゾーンを変更します"
+read -p "terminalの背景を白にします"
+read -p "エディタ(mousepad)の背景を白にし、Favoriteに追加します"
+read -p "エディタ(gedit)の背景を白にし、Favoriteに追加します"
+read -p "FirefoxとChromeをFavoriteに追加します"
+read -p "Chromeの起動オプションに--no-sandboxを追加します"
+read -p "default applicationにFirefoxを指定します"
+read -p "Settings>Power Manger>DisplayおよびSecurityタブからスクリーンセーバーを非有効化します"
+read -p "Firefoxのブラウザアドオンを導入します Foxy Proxy/Wappalyzer/builtwith"
+read -p "Github、TryHackMeのログイン情報を登録します"
 
 read -p "kali/kaliにログインします"
 read -p "sudo passwd root します"
@@ -35,15 +40,10 @@ read -p "Firefoxのブラウザアドオンを導入します Foxy Proxy/Wappaly
 read -p "Burpの証明書をFirefoxに設定します"
 read -p "Github、TryHackMeのログイン情報を登録します"
 
-read -p "rootにログインします"
-read -p "キーボードに日本語を選択します"
-read -p "右上のタイム表示を右クリックし、プロパティでタイムゾーンを変更します"
-read -p "terminalの背景を白にします"
-read -p "エディタ(mousepad)の背景を白にし、Favoriteに追加します"
-read -p "エディタ(gedit)の背景を白にし、Favoriteに追加します"
-read -p "FirefoxとChromeをFavoriteに追加します"
-read -p "Chromeの起動オプションに--no-sandboxを追加します"
-read -p "default applicationにFirefoxを指定します"
-read -p "Settings>Power Manger>DisplayおよびSecurityタブからスクリーンセーバーを非有効化します"
-read -p "Firefoxのブラウザアドオンを導入します Foxy Proxy/Wappalyzer/builtwith"
-read -p "Github、TryHackMeのログイン情報を登録します"
+#echo "chromeの導入"
+#cd /tmp
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#apt install ./google-chrome-stable_current_amd64.deb
+
+#echo "geditの導入"
+#apt install gedit
